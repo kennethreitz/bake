8dbddb1ec23931dc5ccea0a119d6aa8d70301587
Kenneth Reitz <me@kennethreitz.org>
2019-09-18 17:35:57 -0400

    logo

--- bake/tests/conftest.py
+++ bake/tests/conftest.py
@@ -1,11 +1,10 @@
 import os
 
 os.environ["PYTHONUNBUFFERED"] = "1"
+os.environ.pop("BAKEFILE_PATH", "")
 
 import pytest
 import delegator
-
-import bake.cli as bake_cli
 
 
 @pytest.fixture
@@ -15,6 +14,30 @@ def bake():
             os.path.dirname(__file__), "fixtures", f"{fixture}.Bakefile"
         )
         cmd = " ".join(["bake", "-b", bakefile] + list(args))
+        print(f"$ {cmd}")
+
+        c = delegator.run(cmd, block=block)
+
+        if block and assert_ok:
+            assert c.return_code == 0
+
+        if block:
+
+            print(c.out)
+            print(c.err)
+
+        return c
+
+    return run
+
+
+@pytest.fixture
+def bake_i():
+    def run(*args, fixture="default", assert_ok=True, block=True):
+        bakefile = os.path.join(
+            os.path.dirname(__file__), "fixtures", f"{fixture}.Bakefile"
+        )
+        cmd = " ".join(["bake", "-i", "-b", bakefile] + list(args))
         print(f"$ {cmd}")
 
         c = delegator.run(cmd, block=block)
