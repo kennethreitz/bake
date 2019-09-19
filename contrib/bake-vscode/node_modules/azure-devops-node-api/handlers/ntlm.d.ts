c4abdbe33321dd946418e9cadce5bc35bae42601
Kenneth Reitz <me@kennethreitz.org>
2019-09-18 21:33:52 -0400

    test

--- contrib/bake-vscode/node_modules/azure-devops-node-api/handlers/ntlm.d.ts
+++
@@ -1,5 +0,0 @@
-import ifm = require('../interfaces/common/VsoBaseInterfaces');
-import * as resthandlers from 'typed-rest-client/Handlers';
-export declare class NtlmCredentialHandler extends resthandlers.NtlmCredentialHandler implements ifm.IRequestHandler {
-    constructor(username: string, password: string, workstation?: string, domain?: string);
-}
