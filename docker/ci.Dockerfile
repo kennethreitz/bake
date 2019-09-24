de90f5ca5fbae6f6c1a8d2b42620f4785d84de1a
Kenneth Reitz <me@kennethreitz.org>
2019-09-24 08:35:58 -0400

    use python:3-slim-buster

--- docker/ci.Dockerfile
+++ docker/ci.Dockerfile
@@ -5,8 +5,6 @@ ENV TERM xterm
 # -- Install CI deps.
 RUN set -ex && \
     apt-get update -qq && \
-    apt-get upgrade -y -qq && \
-    apt-get install curl -y -qq --no-install-recommends && \
     apt-get install expect npm -y -qq >/dev/null && \
     apt-get clean -y -qq && \
     apt-get autoclean -y -qq && \
@@ -14,7 +12,6 @@ RUN set -ex && \
     npm install -g bats > /dev/null && \
     pip3 install bake-cli --upgrade --quiet > /dev/null && \
     apt remove --autoremove --purge -y curl && \
-    apt-get install expect npm -y -qq >/dev/null && \
     apt-get clean -y -qq && \
     apt-get autoclean -y -qq && \
     rm -fr /var/lib/apt/lists/*
--- docker/scripts/use-mirrors.sh
+++ docker/scripts/use-mirrors.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 SOURCES_LIST='/etc/apt/sources.list'
-PREVIOUS_SOURCES=$(cat $SOURCES_LIST)
 
-perl -pe '/(http|https):\/\/(.*?)(\/|$)/ && s/$2/mirror.math.princeton.edu\/pub\//g' <<< "$PREVIOUS_SOURCES" > "$SOURCES_LIST"
+
+# deb http://snapshot.debian.org/archive/debian/20190910T000000Z buster main
+echo 'deb https://mirror.math.princeton.edu/pub/debian buster main' > ${SOURCES_LIST}
+# deb http://snapshot.debian.org/archive/debian-security/20190910T000000Z buster/updates main
+echo 'deb http://security.debian.org/debian-security buster/updates main' >> ${SOURCES_LIST}
+# deb http://snapshot.debian.org/archive/debian/20190910T000000Z buster-updates main
+echo 'deb http://deb.debian.org/debian buster-updates main' >> ${SOURCES_LIST}
